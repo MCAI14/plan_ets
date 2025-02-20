6ed4aff561e17a9b8d2553cf0565505d27e8f57d
MCAI14 <141061023+MCAI14@users.noreply.github.com>
2025-02-20 14:20:17 +0000

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 def on_overlap_tile(sprite, location):
+    info.change_score_by(1)
+scene.on_overlap_tile(SpriteKind.player,
+    assets.tile("""
+        myTile1
+    """),
+    on_overlap_tile)
 
-scene.on_overlap_tile(SpriteKind.player, myTiles.tile1, on_overlap_tile)
+def on_overlap_tile2(sprite2, location2):
+    for index in range(1):
+        info.change_score_by(-1)
+        scene.camera_shake(4, 500)
+scene.on_overlap_tile(SpriteKind.player,
+    assets.tile("""
+        myTile2
+    """),
+    on_overlap_tile2)
 
 Nave = sprites.create(img("""
-    . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . .
-    e e e e e e e e e e e e e . . . . .
-    e d d d d d d d d d d d e e . . . .
-    e d d d d d d d d d d d d e e . . .
-    e d d 4 4 d d d d 4 4 d d d e e . .
-    e d 4 7 7 4 d d 4 7 7 4 d d d e e e
-    e d 2 7 7 2 d d 2 7 7 2 d d d e e e
-    e d d 2 2 d d d d 2 2 d d d e e . .
-    e d d d d d d d d d d d d e e . . .
-    e d d d d d d d d d d d d e . . . .
-    e e e e e e e e e e e e e e . . . .
-    . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . .
-"""), SpriteKind.player)
-
+        . . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . . 
+            e e e e e e e e e e e e e . . . . . 
+            e d d d d d d d d d d d e e . . . . 
+            e d d d d d d d d d d d d e e . . . 
+            e d d 4 4 d d d d 4 4 d d d e e . . 
+            e d 4 7 7 4 d d 4 7 7 4 d d d e e e 
+            e d 2 7 7 2 d d 2 7 7 2 d d d e e e 
+            e d d 2 2 d d d d 2 2 d d d e e . . 
+            e d d d d d d d d d d d d e e . . . 
+            e d d d d d d d d d d d d e . . . . 
+            e e e e e e e e e e e e e e . . . . 
+            . . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . .
+    """),
+    SpriteKind.player)
 backgroundcolor = 18
-
 scene.set_background_color(backgroundcolor)
-
-game.show_long_text("Bem-vindo ao nosso jogo! Criado pelo teu amigo Cao_Atrevido! ", DialogLayout.CENTER)
-
+game.show_long_text("Bem-vindo ao nosso jogo! Criado pelo teu amigo Cao_Atrevido! ",
+    DialogLayout.CENTER)
+game.show_long_text("Apanha os 14 planetas terra! Ah, e cuidado, porque tanto calor faz mal à saúde!",
+    DialogLayout.CENTER)
 speed = 100
-
 controller.move_sprite(Nave, speed, speed)
-
 scene.camera_follow_sprite(Nave)
-
-tiles.set_current_tilemap(tilemap("""level1"""))
-
+tiles.set_current_tilemap(tilemap("""
+    level1
+"""))
 tiles.place_on_random_tile(Nave, sprites.dungeon.dark_ground_center)
--- main.ts
+++ main.ts
@@ -1,24 +1,34 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    info.changeScoreBy(1)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite2, location2) {
+    for (let index = 0; index < 1; index++) {
+        info.changeScoreBy(-1)
+        scene.cameraShake(4, 500)
+    }
+})
 let Nave = sprites.create(img`
-    . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . .
-    e e e e e e e e e e e e e . . . . .
-    e d d d d d d d d d d d e e . . . .
-    e d d d d d d d d d d d d e e . . .
-    e d d 4 4 d d d d 4 4 d d d e e . .
-    e d 4 7 7 4 d d 4 7 7 4 d d d e e e
-    e d 2 7 7 2 d d 2 7 7 2 d d d e e e
-    e d d 2 2 d d d d 2 2 d d d e e . .
-    e d d d d d d d d d d d d e e . . .
-    e d d d d d d d d d d d d e . . . .
-    e e e e e e e e e e e e e e . . . .
-    . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . .
-`, SpriteKind.Player)
+    . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . 
+    e e e e e e e e e e e e e . . . . . 
+    e d d d d d d d d d d d e e . . . . 
+    e d d d d d d d d d d d d e e . . . 
+    e d d 4 4 d d d d 4 4 d d d e e . . 
+    e d 4 7 7 4 d d 4 7 7 4 d d d e e e 
+    e d 2 7 7 2 d d 2 7 7 2 d d d e e e 
+    e d d 2 2 d d d d 2 2 d d d e e . . 
+    e d d d d d d d d d d d d e e . . . 
+    e d d d d d d d d d d d d e . . . . 
+    e e e e e e e e e e e e e e . . . . 
+    . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
 let backgroundcolor = 18
 scene.setBackgroundColor(backgroundcolor)
 game.showLongText("Bem-vindo ao nosso jogo! Criado pelo teu amigo Cao_Atrevido! ", DialogLayout.Center)
+game.showLongText("Apanha os 14 planetas terra! Ah, e cuidado, porque tanto calor faz mal à saúde!", DialogLayout.Center)
 let speed = 100
 controller.moveSprite(Nave, speed, speed)
 scene.cameraFollowSprite(Nave)
